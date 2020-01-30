c96c8e9a3f2ea455c93d30212bf11c4cdb999377
Nicole Aranzanso <naranzanso@Nicoles-MacBook-Pro.local>
2020-01-30 08:59:11 -0500

    steps

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 //Complete these steps to create a 'signin' prototype:
-
+let signInBtn = document.querySelector (`.signin`)
+let closeBtn = document.querySelector (`.close`)
+let submitBtn = document.querySelector (`.submit`)
+let user = document.querySelector(`#user`)
+let pass = document.querySelector(`#pass`)
 //"Listen" for when the user clicks the #signin button, display the #modal window
 // Make the button function
 // Modal class is the div, change display attribute to ignore display
-let signInBtn = document.querySelector (`.signin`)
+
 
 signInBtn.addEventListener(`click`, (event) => {
 
@@ -14,7 +18,7 @@ signInBtn.addEventListener(`click`, (event) => {
 })
 //"Listen" for when the user clicks the #close button, display (or fade out) out the #modal window
 
-let closeBtn = document.querySelector (`.close`)
+
 
 closeBtn.addEventListener(`click`, (event) => {
 
@@ -24,18 +28,51 @@ closeBtn.addEventListener(`click`, (event) => {
 
 })
 //"Listen" for when the user clicks the #submit button, add an .error class to both input.field elements
-/*let submitBtn = document.querySelector (`.submit`)
+
+/*
 
 submitBtn.addEventListener(`click`, (event) => {
 
   console.log(`Submit input`);
-  let bodyElement.style.input.field.error = `
+  let bodyElement = document.querySelector (`.field`)
+  bodyElement.classList.add = `error`;
+
 })*/
 
 //Easy: First add the .error class to each individually using getElementById() or querySelector()
+
+
+
 //Advanced: Once successful, try applying to both input.field using querySelectorAll() and a forEach() (or another type of traversal loop)
 //Remove the .error class from any individual <input> when the cursor gives it focus
 //Easy: First try writing one event listener/handler per input element
 //Advanced: Then do this by writing only ONE event listener/handler for all input elements and applying it using a traversal loop
 
-//Test as you go!!!!
+// add class .error to #user
+
+
+
+user.classList.add(`.error`)
+
+user.classList.remove(`.error`)
+
+// add class .error to #pass
+
+
+pass.classList.add(`.error`)
+
+pass.classList.remove(`.error`)
+
+// clickable .submit
+
+// remove class .error to #user
+
+// remove class .error to #pass
+
+// focus #user
+
+user.addEventListener(`focus`, (event) => { console.log(`focus!`) })
+
+// focus #pass
+
+pass.addEventListener(`focus`, (event) => { console.log(`focus!`) })
